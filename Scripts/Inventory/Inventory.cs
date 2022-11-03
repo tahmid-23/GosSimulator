7cd4fab87e758e4efa91887cebb116fed39c2ed2
PranjalChaitanya <44535826+PranjalChaitanya@users.noreply.github.com>
2022-11-03 04:01:06 -0400

    Bogus Basic Inventory System
    Nonsense

---
+++ Scripts/Inventory/Inventory.cs
@@ -0,0 +1,12 @@
+namespace DefaultNamespace
+{
+    public class Inventory
+    {
+        private Weapons weapon1 = null;
+        private Weapons weapon2 = null;
+        private Weapons weapon1 = null;
+        private OtherItems otherItem1 = null;
+        private OtherItems otherItem2 = null;
+        private OtherItems otherItem3 = null;
+    }
+}

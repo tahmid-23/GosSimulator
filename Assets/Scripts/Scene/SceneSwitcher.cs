689be21fc04819a8a6802884605fd7922bdeebfa
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-11-05 04:19:00 -0400

    move SceneSwitcher

--- Assets/Scripts/Scene/SceneSwitcher.cs
+++ Assets/Scripts/Scene/SceneSwitcher.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace Scenes
+namespace Scene
 {
     public class SceneSwitcher : MonoBehaviour
     {

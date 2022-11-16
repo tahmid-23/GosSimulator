8a3fb26b74e0624e67b4ca3ea80f0f01d0be9787
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-11-15 20:11:17 -0500

    add MovementController delegate

--- Assets/Scripts/Movement/MovementController.cs
+++ Assets/Scripts/Movement/MovementController.cs
@@ -4,12 +4,16 @@ namespace Movement
 {
     public class MovementController : MonoBehaviour
     {
+
+        public CollisionHandler OnCollision;
         
         public Vector2 Speed { get; set; } = Vector2.zero;
 
         private BoxCollider2D _boxCollider2D;
 
         private Rigidbody2D _rigidbody2D;
+
+        public delegate void CollisionHandler(RaycastHit2D collisionRaycast);
 
         private void Awake()
         {
@@ -25,6 +29,8 @@ namespace Movement
             
             RaycastHit2D[] raycasts = Physics2D.BoxCastAll(center, gosTransform.localScale * _boxCollider2D.size,
                 0F, resultSpeed.normalized, resultSpeed.magnitude);
+            RaycastHit2D[] collidingRaycasts = new RaycastHit2D[raycasts.Length];
+            int collidingRaycastCount = 0;
             foreach (RaycastHit2D raycast in raycasts)
             {
                 if (raycast.collider == null || raycast.collider == _boxCollider2D) continue;
@@ -35,9 +41,15 @@ namespace Movement
                 Vector2 reduction = (projection + raycast.distance) * raycast.normal;
                 resultSpeed -= reduction;
                 Speed -= projection / Time.fixedDeltaTime * raycast.normal; // next speed should be completely zeroed
+
+                collidingRaycasts[collidingRaycastCount++] = raycast;
             }
-            
+
             _rigidbody2D.MovePosition(_rigidbody2D.position + resultSpeed);
+            foreach (RaycastHit2D raycast in collidingRaycasts)
+            {
+                OnCollision(raycast);
+            }
         }
 
     }
